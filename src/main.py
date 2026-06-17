80e0559fa0741387fa90524f95ead2fe5af60f82
affection7 <salkow12345@gmail.com>
2026-06-17 10:50:57 +0300

    feat: add main etl script loading customers to stg

---
+++ src/main.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import psycopg2
+from psycopg2.extras import execute_values
+
+DATABASE_URL = "postgresql://postgres:1234@127.0.0.1:5432/postgres"
+
+with open('./ddl/staging/create_stg_customers.sql', encoding='utf-8') as f:
+    sql_create = f.read()
+
+df = pd.read_csv('./data/customers.csv')
+
+rows = df[['customer_id', 'full_name', 'email', 'phone', 'city', 'created_at']].values.tolist()
+
+conn = psycopg2.connect(DATABASE_URL)
+cursor = conn.cursor()
+
+cursor.execute(sql_create)
+
+sql_insert = './ddl/staging/insert_stg_customers.sql'
+
+with open(sql_insert, encoding='utf-8') as f:
+    sql_insert = f.read()
+
+
+execute_values(cursor, sql_insert, rows)
+
+conn.commit()
+cursor.close()
+conn.close()
+
+print(f"Успешно загружено {len(rows)} строк")
