1bed879067eb07098a3b5386867ae8badb8b6dde
affection7 <salkow12345@gmail.com>
2026-06-17 13:00:23 +0300

    feat: add orders table to stg

--- src/main.py
+++ src/main.py
@@ -5,31 +5,56 @@ from psycopg2.extras import execute_values
 from dotenv import load_dotenv
 
 load_dotenv()
-
 DATABASE_URL = os.getenv("DATABASE_URL")
-
-with open('./ddl/staging/create_stg_customers.sql', encoding='utf-8') as f:
-    sql_create = f.read()
-
-df = pd.read_csv('./data/customers.csv')
-
-rows = df[['customer_id', 'full_name', 'email', 'phone', 'city', 'created_at']].values.tolist()
 
 conn = psycopg2.connect(DATABASE_URL)
 cursor = conn.cursor()
 
-cursor.execute(sql_create)
+with open('./ddl/staging/create_stg_customers.sql', encoding='utf-8') as f:
+    sql_create_cus = f.read()
+cursor.execute(sql_create_cus)
 
-sql_insert = './ddl/staging/insert_stg_customers.sql'
+with open('./ddl/staging/create_stg_events.sql', encoding='utf-8') as f:
+    sql_create_events = f.read()
+cursor.execute(sql_create_events)
 
-with open(sql_insert, encoding='utf-8') as f:
+with open('./ddl/staging/create_stg_orders.sql', encoding='utf-8') as f:
+    sql_create_orders = f.read()
+cursor.execute(sql_create_orders)
+
+with open('./ddl/staging/create_stg_payments.sql', encoding='utf-8') as f:
+    sql_create_pay = f.read()
+cursor.execute(sql_create_pay)
+
+with open('./ddl/staging/create_stg_products.sql', encoding='utf-8') as f:
+    sql_create_prod = f.read()
+cursor.execute(sql_create_prod)
+
+
+
+df_cust = pd.read_csv('./data/customers.csv')
+rows_cust = df_cust[['customer_id', 'full_name', 'email', 'phone', 'city', 'created_at']].values.tolist()
+
+sql_insert_cust = './ddl/staging/insert_stg_customers.sql'
+
+with open(sql_insert_cust, encoding='utf-8') as f:
     sql_insert = f.read()
 
+execute_values(cursor, sql_insert, rows_cust)
 
-execute_values(cursor, sql_insert, rows)
+df_orders = pd.read_json('./data/orders.json')
+rows_orders= df_orders[['order_id', 'customer_id', 'product_id', 'quantity', 'unit_price', 'currency', 'order_timestamp', 'status']].values.tolist()
+
+sql_insert_orders= './ddl/staging/insert_stg_orders.sql'
+
+with open(sql_insert_orders, encoding='utf-8') as f:
+    sql_insert_orders = f.read()
+
+execute_values(cursor, sql_insert_orders, rows_orders)
 
 conn.commit()
 cursor.close()
 conn.close()
 
-print(f"Успешно загружено {len(rows)} строк")
+print(f"Успешно загружено {len(rows_cust)} строк в таблицу customers")
+print(f"Успешно загружено {len(rows_orders)} строк в таблицу  orders")
