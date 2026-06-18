d168c36b6d06b8d158c0c984e7772a61d83ba34f
affection7 <salkow12345@gmail.com>
2026-06-18 11:28:02 +0300

    refactor: del upsert methode

--- ddl/staging/insert_stg_products.sql
+++ ddl/staging/insert_stg_products.sql
@@ -1,1 +1,1 @@
-INSERT INTO staging.products (product_id, product_name, category, price, currency, is_active) VALUES %s ON CONFLICT (product_id) DO NOTHING;
+INSERT INTO staging.products (product_id, product_name, category, price, currency, is_active) VALUES %s;
