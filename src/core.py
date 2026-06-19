9e1f4f4a36e0ed2d1b619fcdcf0aa9247c7e8e36
affection7 <salkow12345@gmail.com>
2026-06-19 06:08:06 +0300

    fix: fixed core reads from staging

--- src/core.py
+++ src/core.py
@@ -1,11 +1,18 @@
 import psycopg2
 import os
+import pandas as  pd
 from psycopg2.extras import execute_values
 from dotenv import load_dotenv
 from validate import clean_customers, clean_products, clean_orders, clean_payments, clean_events
 
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_IP = os.getenv("DB_IP")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_IP}:{DB_PORT}/{DB_NAME}"
 
 def execute_sql(cursor, path):
     with open(path, encoding='utf-8') as f:
@@ -18,6 +25,12 @@ def load_table(cursor, table, df):
         execute_values(cursor, f.read(), rows)
     print(f"Успешно загружено {len(rows)} строк в таблицу {table}")
 
+def read_staging_table(cursor, table_name):
+    cursor.execute(f"SELECT * FROM staging.{table_name}")
+    rows = cursor.fetchall()
+    columns = [desc[0] for desc in cursor.description]
+    return pd.DataFrame(rows, columns=columns)
+
 conn = psycopg2.connect(DATABASE_URL)
 cursor = conn.cursor()
 
@@ -27,11 +40,25 @@ execute_sql(cursor, './ddl/core/create_core_orders.sql')
 execute_sql(cursor, './ddl/core/create_core_payments.sql')
 execute_sql(cursor, './ddl/core/create_core_events.sql')
 
-load_table(cursor, 'customers', clean_customers(), ['customer_id', 'full_name', 'email', 'phone', 'city', 'created_at'])
-load_table(cursor, 'products', clean_products(), ['product_id', 'product_name', 'category', 'price', 'currency', 'is_active'])
-load_table(cursor, 'orders', clean_orders(), ['order_id', 'customer_id', 'product_id', 'quantity', 'unit_price', 'currency', 'order_timestamp', 'status'])
-load_table(cursor, 'payments', clean_payments(), ['payment_id', 'order_id', 'payment_method', 'amount', 'currency', 'payment_timestamp'])
-load_table(cursor, 'events', clean_events(), ['event_id', 'customer_id', 'event_type', 'event_timestamp', 'product_id'])
+df = read_staging_table(cursor, 'customers')
+df = clean_customers(df)
+load_table(cursor, 'customers', df)
+
+df = read_staging_table(cursor, 'orders')
+df = clean_orders(df)
+load_table(cursor, 'orders', df)
+
+df = read_staging_table(cursor, 'payments')
+df = clean_payments(df)
+load_table(cursor, 'payments', df)
+
+df = read_staging_table(cursor, 'products')
+df = clean_products(df)
+load_table(cursor, 'products', df)
+
+df = read_staging_table(cursor, 'events')
+df = clean_events(df)
+load_table(cursor, 'events', df)
 
 conn.commit()
 cursor.close()
