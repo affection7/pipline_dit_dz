1e847cbddd8e7965d2792d1b89632996be010f31
affection7 <salkow12345@gmail.com>
2026-06-19 06:13:34 +0300

    feat: add  FK constraint core schema

--- ddl/core/create_core_customers.sql
+++ ddl/core/create_core_customers.sql
@@ -1,9 +1,9 @@
 CREATE SCHEMA IF NOT EXISTS core;
 CREATE TABLE IF NOT EXISTS core.customers (
     customer_id INTEGER PRIMARY KEY,
-    full_name VARCHAR(25),
-    email VARCHAR(25),
-    phone VARCHAR(15),
-    city VARCHAR(25),
+    full_name VARCHAR(50),
+    email VARCHAR(50),
+    phone VARCHAR(50),
+    city VARCHAR(50),
     created_at DATE
 );
--- ddl/core/create_core_events.sql
+++ ddl/core/create_core_events.sql
@@ -1,10 +1,10 @@
 CREATE SCHEMA IF NOT EXISTS core;
 CREATE TABLE IF NOT EXISTS core.events (
-    event_id INTEGER PRIMARY KEY,
-    customer_id INTEGER,
+    event_id VARCHAR(50) PRIMARY KEY,
+    customer_id VARCHAR(50),
     event_type VARCHAR(25),
     event_timestamp TIMESTAMP,
-    product_id INTEGER,
+    product_id VARCHAR(50),
     CONSTRAINT fk_events_customer FOREIGN KEY (customer_id) REFERENCES core.customers(customer_id),
     CONSTRAINT fk_events_product FOREIGN KEY (product_id) REFERENCES core.products(product_id)
 );
